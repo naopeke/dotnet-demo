1c4960f47824e0a7bd6da7f369c0848b1d9a48d7
naopeke <naopeke@protonmail.com>
2024-05-10 10:37:14 +0200

    05_03

--- Ch05/05_03/Begin/Summaries/ClientApp/src/app/components/nav-menu/nav-menu.component.ts
+++ Ch05/05_03/Begin/Summaries/ClientApp/src/app/components/nav-menu/nav-menu.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit{
   isExpanded = false;
+
+  constructor(private auth: AuthService){};
+
+  ngOnInit(){
+    this.auth.handleAuthCallback();
+  }
 
   collapse() {
     this.isExpanded = false;
